beb8011b9d57cb90713a6a6f3ae4f8a2a63dd9cb
Brendan Hanna <gepetto503@gmail.com>
2017-09-18 16:18:08 -0700

    nested components.  ticket within ticketlist within app.  still no live reload

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
+//index.jsx is the entry point, a special type of file.  it's only job is to render the parent component (in this case, App.jsx).  react can only render one component at a time, but that component may have limitless children and grandchildren
+
 //imports React libraries from npm devDependencies (found in package.json)
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+
+
 
 ReactDOM.render(
   //what to render
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3a</h3>
-    <h3>Thato and Haely</h3>
-    <p><em>Firebase won't save record</em></p>
-  </div>,
+  <App/>,
   //where to render it
   document.getElementById('react-app-root')
 );
